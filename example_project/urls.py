27d68b187293516120928fc4dfee0f99284492bd
Eshaan Bansal <eshaan7bansal@gmail.com>
2021-05-14 19:42:22 +0530

    example_project: add ThrottledView, DEFAULT_AUTO_FIELD, DEFAULT_THROTTLE_RATES, enable admin site

--- example_project/urls.py
+++ example_project/urls.py
@@ -1,9 +1,14 @@
-from django.urls import include, re_path
+from django.urls import include, re_path, path
+from django.views.generic.base import RedirectView
+from django.contrib import admin
 
-from .views import CachedRootView, RootView
+from .views import CachedRootView, RootView, ThrottledView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="admin/", permanent=False)),
+    path("admin/", admin.site.urls, name="admin"),
     re_path(r"^api/", include("durin.urls")),
     re_path(r"^api/$", RootView.as_view(), name="api-root"),
     re_path(r"^api/cached$", CachedRootView.as_view(), name="cached-auth-api"),
+    re_path(r"^api/throttled$", ThrottledView.as_view(), name="throttled-api"),
 ]
--- example_project/views.py
+++ example_project/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from durin.auth import CachedTokenAuthentication, TokenAuthentication
+from durin.throttling import UserClientRateThrottle
 
 
 class RootView(APIView):
@@ -19,3 +20,12 @@ class CachedRootView(APIView):
 
     def get(self, request):
         return Response("cached api root")
+
+
+class ThrottledView(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    throttle_classes = (UserClientRateThrottle,)
+
+    def get(self, request):
+        return Response("ThrottledView")
