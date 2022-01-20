4c933cfea7f7f2b6084dcd0795be2e1ac3d22309
Eshaan Bansal <eshaan7bansal@gmail.com>
2022-01-20 16:39:22 +0530

    tests: enable tox matrix for django 4.0 (#26)

--- durin/auth.py
+++ durin/auth.py
@@ -1,4 +1,4 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from rest_framework import exceptions
 from rest_framework.authentication import BaseAuthentication, get_authorization_header
 
--- durin/urls.py
+++ durin/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import re_path
 
 from durin import views
 
 urlpatterns = [
-    url(r"login/", views.LoginView.as_view(), name="durin_login"),
-    url(r"refresh/", views.RefreshView.as_view(), name="durin_refresh"),
-    url(r"logout/", views.LogoutView.as_view(), name="durin_logout"),
-    url(r"logoutall/", views.LogoutAllView.as_view(), name="durin_logoutall"),
+    re_path(r"login/", views.LoginView.as_view(), name="durin_login"),
+    re_path(r"refresh/", views.RefreshView.as_view(), name="durin_refresh"),
+    re_path(r"logout/", views.LogoutView.as_view(), name="durin_logout"),
+    re_path(r"logoutall/", views.LogoutAllView.as_view(), name="durin_logoutall"),
 ]
